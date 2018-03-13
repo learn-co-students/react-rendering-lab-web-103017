d15f5f8821cfbb4b66717fc8121f5e7ffa5c423f
psarma89 <priyam.sarma@gmail.com>
2018-03-13 10:29:48 -0400

    Done.

--- src/components/Pikachu.js
+++ src/components/Pikachu.js
@@ -10,6 +10,10 @@ class Pikachu extends React.Component {
     };
   }
 
+  componentDidUpdate(nextProps, nextState){
+    this.resizePikachu()
+  }
+
   resizePikachu = () => {
     const pikachu = document.getElementById("pikachu");
     pikachu.height = this.state.size;
